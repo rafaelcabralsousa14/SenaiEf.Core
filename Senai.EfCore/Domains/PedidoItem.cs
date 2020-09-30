a2a7fa899df5f1c375df078d390d69b30d63cb67
rafaelcabralsousa14 <61585019+rafaelcabralsousa14@users.noreply.github.com>
2020-09-30 17:59:21 -0300

    Swagger

---
+++ Senai.EfCore/Domains/PedidoItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.EfCore.Domains
+{
+    public class PedidoItem
+    {
+        [Key]
+        public Guid IdPedidoItem { get; set; }
+        
+        public Guid IdPedido { get; set; }
+        [ForeignKey("IdPedido")]
+        public Pedido Pedido { get; set; }
+
+        public Guid IdProduto { get; set; }
+        [ForeignKey("IdProduto")]
+        public Produto Produto { get; set; }
+
+        [Required]
+        public int Quantidade { get; set; }
+
+        public PedidoItem()
+        {
+            IdPedidoItem = Guid.NewGuid();
+            IdPedido = Guid.NewGuid();
+            IdProduto = Guid.NewGuid();
+        }
+    }
+}
---
+++ Senai.EfCore/Domains/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.EfCore.Domains
+{
+    public class Produto
+    {
+        [Key]
+        public Guid IdProduto { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        public Produto()
+        {
+            IdProduto = Guid.NewGuid();
+        }
+    }
+}
